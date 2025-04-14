eb5bd933f4c60e8de187621014736e7cd45e2d57
Lungisani Nhlengethwa <LungisaniN@hollywoodbets.net>
2025-04-14 13:47:25 +0200

    vite repairs

--- projects/javascript/vite.config.js
+++ projects/javascript/vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'public', // Tell Vite the project root is the 'public' directory
+  root: 'public', 
   build: {
-    // Adjust output directory if needed, as it's relative to the new root
-    outDir: '../dist', // Example: output to 'dist' outside 'public'
+    outDir: '../dist', 
     rollupOptions: {
-       // Input might need adjustment relative to the new root
        input: '/index.html',
-    }
+    },
+    emptyOutDir: true,
   }
 }) 

72c9d135acb08e9cd013d42fb7682efe1d868f04
Lungisani Nhlengethwa <LungisaniN@hollywoodbets.net>
2025-04-23 11:27:38 +0200

    main

--- projects/javascript/src/components/Checkbox.js
+++ projects/javascript/src/components/Checkbox.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Checkbox({ name, options, onChange }) {
-  return (
-    <div className="form-group">
-      {options.map((option) => (
-        <div key={option.value} className="form-check">
-          <input
-            type="checkbox"
-            id={`${name}-${option.value}`}
-            name={name}
-            value={option.value}
-            onChange={onChange}
-            className="form-check-input"
-          />
-          <label htmlFor={`${name}-${option.value}`} className="form-check-label">
-            {option.label}
-          </label>
-        </div>
-      ))}
-    </div>
+  return React.createElement(
+    'div',
+    { className: 'form-group' },
+    options.map((option) =>
+      React.createElement(
+        'div',
+        { key: option.value, className: 'form-check' },
+        React.createElement('input', {
+          type: 'checkbox',
+          id: `${name}-${option.value}`,
+          name: name,
+          value: option.value,
+          onChange: onChange,
+          className: 'form-check-input',
+        }),
+        React.createElement(
+          'label',
+          { htmlFor: `${name}-${option.value}`, className: 'form-check-label' },
+          option.label
+        )
+      )
+    )
   );
 }
+
+Checkbox.propTypes = {
+  name: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.any.isRequired,
+    label: PropTypes.string.isRequired,
+  })).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default Checkbox;
